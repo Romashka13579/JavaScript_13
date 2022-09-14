7c269650d1ba8e0e991151d68de10b6daba03f8e
Romashka <rroma13579@gmail.com>
2022-09-14 21:42:50 +0200

    seventh commit

--- js/script.js
+++ js/script.js
@@ -5,34 +5,18 @@ var audio_sources = document.querySelectorAll(".audio-source");
 var song_played = 0;
 let swaper;
 
+var firstClone = songs[0].cloneNode(true);
+var lastClone = songs[songs.length - 1].cloneNode(true);
+
+songs.append(firstClone);
+songs.prepend(lastClone);
+
 btn_next.addEventListener('click', (e) => {
     e.preventDefault();
-    song_played++;
-    if(song_played < songs.length){
-        songs[song_played-1].classList.add("song-display");
-        songs[song_played].classList.remove("song-display");
-        swaper = audio_sources[song_played-1];
-        audio_sources[song_played-1] = audio_sources[song_played];
-        audio_sources[song_played] = swaper;
-        audio_sources = document.querySelectorAll(".audio-source");
-    }
-    else{
-        songs[song_played-1].classList.add("song-display");
-        song_played = 0;
-        songs[song_played].classList.remove("song-display");
-    }
 });
 
 btn_prev.addEventListener('click', (e) => {
     e.preventDefault();
-    song_played--;
-    if(song_played > -1){
-        songs[song_played].classList.remove("song-display");
-        songs[song_played+1].classList.add("song-display");
-    }
-    else{
-        songs[song_played+1].classList.add("song-display");
-        song_played = 4;
-        songs[song_played].classList.remove("song-display");
-    }
+
 });
+
