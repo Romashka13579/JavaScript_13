c1d36b99f08375c7447e65ffdedc4735b9be1f58
Romashka <rroma13579@gmail.com>
2022-09-17 18:54:20 +0200

    eleventh commit

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ playerFull.prepend(lastClone);
 
 const songWidth = playerOverflow.clientWidth;
 
-playerFull.style.transform = `translateX(${-songWidth * i}px)`;
+playerFull.style.left = "" + (-songWidth * i) + "px";
 
 songs = document.querySelectorAll(".song");
 
@@ -39,7 +39,7 @@ playerFull.addEventListener('transitionend', () => {
         songs[songs.length - 1].style.width = "240px";
         songs[songs.length - 1].style.height = "240px";
         songs[songs.length - 1].style.margin = "70px";
-        playerFull.style.transform = `translateX(${-songWidth * i}px)`;
+        playerFull.style.left = "" + -songWidth * i + "px";
     }
     songs = document.querySelectorAll(".song");
     if (songs[i].id === lastClone.id) {
@@ -52,39 +52,41 @@ playerFull.addEventListener('transitionend', () => {
         songs[1].style.width = "240px";
         songs[1].style.height = "240px";
         songs[1].style.margin = "70px";
-        playerFull.style.transform = `translateX(${-songWidth * i}px)`;
+        playerFull.style.left = "" + (-songWidth * i) + "px";
     }
 });
 
 btn_next.addEventListener('click', () => {
     songs = document.querySelectorAll(".song");
-    if(i >= songs.length - 1){return;}
+    if (i >= songs.length - 1) { return; }
     i++;
-    playerFull.style.transform = `translateX(${-songWidth * i}px)`;
+    playerFull.style.left = "" + (-songWidth * i) + "px";
     songs[i].style.width = "300px";
     songs[i].style.height = "300px";
     songs[i].style.margin = "40px";
-    songs[i-1].style.width = "240px";
-    songs[i-1].style.height = "240px";
-    songs[i-1].style.margin = "70px";
+    songs[i - 1].style.width = "240px";
+    songs[i - 1].style.height = "240px";
+    songs[i - 1].style.margin = "70px";
     songs[i].style.transition = ".7s";
-    songs[i-1].style.transition = ".7s";
+    songs[i - 1].style.transition = ".7s";
     playerFull.style.transition = ".7s";
 });
 
 btn_prev.addEventListener('click', () => {
     songs = document.querySelectorAll(".song");
-    if(i <= 0){return;}
+    if (i <= 0) { return; }
+
     i--;
     songs[i].style.width = "300px";
     songs[i].style.height = "300px";
     songs[i].style.margin = "40px";
-    songs[i+1].style.width = "240px";
-    songs[i+1].style.height = "240px";
-    songs[i+1].style.margin = "70px";
-    playerFull.style.transform = `translateX(${-songWidth * i}px)`;
+    songs[i + 1].style.width = "240px";
+    songs[i + 1].style.height = "240px";
+    songs[i + 1].style.margin = "70px";
+    playerFull.style.left = "" + (-songWidth * i) + "px";
     playerFull.style.transition = ".7s";
     songs[i].style.transition = ".7s";
-    songs[i+1].style.transition = ".7s";
+    songs[i + 1].style.transition = ".7s";
+
 });
 
